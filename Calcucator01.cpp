86e610ee163d4659c4c41ccf507aa9a3967e764f
Евгений Лактионов <laktionov_e@t.top-academy.ru>
2025-09-04 12:37:08 +0300

    Arithmetic

--- Calcucator01.cpp
+++ Calcucator01.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 void errorMessage(const string& message);
+double compute(double a, double b, const string& op);
 
 int main() {
     setlocale(LC_ALL, "ru");
@@ -16,6 +17,19 @@ int main() {
         return 1;
     }
 
+    std::string op;
+    std::string allowed[] = { "+", "-", "*", "/" };
+
+    std::print("Введи операцию (+ - * /): ");
+    cin >> op;
+
+    if (std::find(std::begin(allowed), std::end(allowed), op)
+        == std::end(allowed)) {
+        errorMessage("Ошибка: неизвестная операция");
+        cin.get();
+        return 1;
+    }
+
     std::print("Введи второе число: ");
     double b;
     if (!(cin >> b)) {
@@ -23,8 +37,16 @@ int main() {
         cin.get();
         return 1;
     }
-
-    std::println("{} + {} = {}", a, b, a + b);
+    
+    double result = compute(a, b, op);
+    if (std::isnan(result)) 
+    {
+        std::println("Результат некорректен");
+    }
+    else 
+    {
+        std::println("{} {} {} = {}", a, op, b, result);
+    }
 
     cin.get();
     cin.get();
@@ -36,4 +58,13 @@ void errorMessage(const string& message)
     std::println("{}", message);
     cin.clear();
     cin.ignore(1000, '\n');
+}
+
+double compute(double a, double b, const std::string& op) {
+    if (op == "+") return a + b;
+    if (op == "-") return a - b;
+    if (op == "*") return a * b;
+    if (op == "/") return (b != 0) ? a / b : std::nan("");
+
+    return std::nan(""); // неизвестная операция
 }
